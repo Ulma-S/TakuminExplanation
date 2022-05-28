4c4124dea2a2513a2333bb0fd571e0e91f378847
Ulma-S <shunjinomaku1@gmail.com>
2022-05-28 23:55:22 +0900

    Initial commit.

--- Assets/Lessons/SceneTransition/Scripts/RutimeInitializer.cs
+++ Assets/Lessons/SceneTransition/Scripts/RutimeInitializer.cs
@@ -5,20 +5,22 @@ namespace Ulma
 {
     public static class RutimeInitializer
     {
+        private const string ResidentSceneName = "Resident";
+
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static void LoadPermanentScene()
         {
             //常駐シーンがロードされていなければロードする.
             for (int i = 0; i < UnitySceneManager.sceneCount; i++)
             {
-                if (UnitySceneManager.GetSceneAt(i).name == "Resident")
+                if (UnitySceneManager.GetSceneAt(i).name == ResidentSceneName)
                 {
                     break;
                 }
 
                 if (i == UnitySceneManager.sceneCount - 1)
                 {
-                    UnitySceneManager.LoadScene("Resident", UnityEngine.SceneManagement.LoadSceneMode.Additive);
+                    UnitySceneManager.LoadScene(ResidentSceneName, UnityEngine.SceneManagement.LoadSceneMode.Additive);
                 }
             }
         }
